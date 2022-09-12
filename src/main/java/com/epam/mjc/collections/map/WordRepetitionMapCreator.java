356d96a93d85669dd01e27bb75143c98a86db578
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-09-12 15:48:20 +0500

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -7,6 +7,10 @@ public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
         sentence = sentence.toLowerCase();
         Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) >= 97 && sentence.charAt(i) <= 122 || sentence.charAt(i) == 32){}
+            else sentence.replace(sentence.charAt(i)+"","");
+        }
         for (String str : sentence.split(" ")) {
             if(map.containsKey(str))
                 map.put(str, map.get(str) + 1);
